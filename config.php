b13a4178ca9d228dfd9f986f3684a40ed1767ab8
bluu1296 <binhluu96@gmail.com>
2017-12-15 09:06:43 +0100

    Mappenstructuur aangepast en opgemaakt. Home pagina en dergelijke pagina's toegevoegd.

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+ 
+ 
+$config = array(
+    "db" => array(
+        "db1" => array(
+            "dbname" => "winkel",
+            "username" => "root",
+            "password" => "",
+            "host" => "localhost"
+        ),
+    ),
+    "paths" => array(
+        "resources" => "/resources",
+		"style" => "public_html/css/style.css",
+		"bootstrap" => "public_html/js/bootstrap.js",
+        "images" => $_SERVER["DOCUMENT_ROOT"] . "/img/",
+        
+    )
+);
+define('TEMPLATES_PATH', realpath(dirname(__FILE__) . '/resources/templates'));
+defined("LIBRARY_PATH")
+    or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
+     
+//defined("TEMPLATES_PATH")
+//    or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/resources/templates'));
+
+ini_set("error_reporting", "true");
+error_reporting(E_ALL|E_STRCT);
--- index.php
+++ index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Test</title>
-    </head>
-    <body>
-		
-		
-		
-    </body>
-</html>
+<?php
+
+require_once 'config.php';
+
+require_once TEMPLATES_PATH . '/header.php';
+
+
+//$page = trim($_SERVER['REQUEST_URI'], '/');
+
+
+
+include 'pages/home.php';
+//include 'pages/contact.php';
+
+
+require_once TEMPLATES_PATH . '/footer.php';
