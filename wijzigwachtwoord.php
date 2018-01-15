e08b6fe1fc5cf763aa472f5c25121bc9481ab91d
Timo Lubberts <33627161+timolubberts@users.noreply.github.com>
2018-01-15 13:21:09 +0100

    Add files via upload
    wijzig wachtwoord pagina en accountbeheer pagina

---
+++ wijzigwachtwoord.php
@@ -0,0 +1,6 @@
+<form action="./controller/wijzigwachtwoordController" method="post">
+    Oude wachtwoord<input type="password" name="oudww"><br>
+    Nieuwe wachtwoord<input type="password" name="nieuwww"><br>
+    Herhaal nieuw wachtwoord<input type="password" name="nieuwwwh"><br>
+    <input type="submit" value="Opslaan">
+</form>
