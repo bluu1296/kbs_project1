a7224241b09f1c61ce32752ce7e3efd8d2fd2aa6
fuuad <33627186+fuuad@users.noreply.github.com>
2018-01-29 00:58:57 +0100

    Maken afspraak back-end

--- pages/afspraak.php
+++ pages/afspraak.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        
-        ?>
-    </body>
-</html>
+<div class="container">
+    <h2>Afspraak maken</h2>
+    <form  method="post">
+        <h4>Voornaam</h4>
+        <input type="text" name="voornaam" placeholder="Piet" required>
+        <h4>Achternaam</h4>
+        <input type="text" name="achternaam" placeholder="Hendriks" required>
+        <h4>E-mail adres</h4>
+        <input type="email" name="E-mail" placeholder="iemand@example.com" required autofocus> 
+        <h4>Tijdstip</h4>
+        <input type="datetime-local" name="Datum" placeholder="" required>
+        <h4> Telefoonnummer </h4> 
+        <input type="text" name="telnummer" placeholder="0612345678" required><br>
+        <br><input type="submit" value="Afspraak maken">
+    </form>
+</div>
