0a3e6578d494dbc25039f0a36a38248fe588514e
fuuad <33627186+fuuad@users.noreply.github.com>
2018-01-29 13:53:41 +0100

    Test afspraakcontroller.php
    (Werkt niet)

--- controller/afspraakController.php
+++ controller/afspraakController.php
@@ -13,12 +13,37 @@
  */
 class Afspraak {
 
-	public function __construct() {
-		require_once './pages/afspraak.php';
-		$this->_other();
-	}
-	protected function _other() {
-		
-	}
-	
+    public function __construct() {
+        require_once './pages/afspraak.php';
+        $this->_other();
+    }
+
+    protected function _other() {
+        include 'DB-connectie.php';
+
+        $vak = filter_input(INPUT_POST, "vak");
+        $email = filter_input(INPUT_POST, "email");
+        $begintijd = filter_input(INPUT_POST, "begintijd");
+        $eindtijd = filter_input(INPUT_POST, "eindtijd");
+
+
+        $transStart = $pdo->prepare("START TRANSACTION"); //start transactie
+
+        $afspraakmakenA = $pdo->prepare("INSERT INTO `afspraak` (gebruiker_id) SELECT gebruiker_id FROM gebruiker WHERE email = " . $email . "");
+        $afspraakmakenB = $pdo->prepare("INSERT INTO `afspraak` (vak_id) SELECT vak_id FROM vak WHERE naam = " . $vak . "");
+        $afspraakmakenC = $pdo->prepare("INSERT INTO `afspraak` (begintijd, eindtijd) VALUES (" . $begintijd . ", " . $eindtijd . "");
+        $afspraakmakenD = $pdo->prepare("SELECT * FROM gebruiker");
+
+        $transCommit = $pdo->prepare("COMMIT"); //commit
+
+        $transStart->execute();
+        $afspraakmakenA->execute();
+        $afspraakmakenB->execute();
+        $afspraakmakenC->execute();
+        $afspraakmakenD->execute();
+        $transCommit->execute();
+        
+        print_r($afspraakmakenD);
+    }
+
 }
